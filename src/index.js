7652525bff2eb82fde1aac3a79216f7120d8b095
JohnDJake <john.d.jake@gmail.com>
2021-02-20 23:23:11 -0800

    styling

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
